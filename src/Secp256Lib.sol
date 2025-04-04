2d539d1502b04c6a4bcab800e97ff27efca44dd0
Milos Djurica <milos.djurica@mvpworkshop.co>
2025-04-04 12:25:59 +0200

    isInfinity() and isOnCurve() methods

--- src/Secp256Lib.sol
+++ src/Secp256Lib.sol
@@ -8,8 +8,20 @@ library Secp256k1 {
     // p = 2^256 - 2^32 - 977
     uint256 constant p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
 
-    function isOnCurve() internal {}
-    function isInfinity() internal {}
+    // Check if the point is infinity
+    function isInfinity(uint256 x, uint256 y) internal pure returns (bool) {
+        return x == 0 && y == 0;
+    }
+
+    // y^2 ≡ x^3 + 7 (mod p)
+    // Check if the point is on curve
+    function isOnCurve(uint256 x, uint256 y) internal pure returns (bool) {
+        uint256 leftHandSide = mulmod(y, y, p); // y^2 mod p
+        uint256 xCubed = mulmod(x, mulmod(x, x, p), p); // x^3 mod p
+        uint256 rightHandSide = addmod(xCubed, 7, p); // (x*3 + 8) mod p
+        return leftHandSide == rightHandSide;
+    }
+
     function addPoint() internal {}
     function negatePoint() internal {}
     function orderOfPoint() internal {}
