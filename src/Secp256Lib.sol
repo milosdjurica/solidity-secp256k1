c2fc8cae0ab7cdfdedc3fa3e4d11fac000794b61
Milos Djurica <milos.djurica@mvpworkshop.co>
2025-04-04 13:59:39 +0200

    onlyValidPoint modifier and unchecked methods

--- src/Secp256Lib.sol
+++ src/Secp256Lib.sol
@@ -10,6 +10,11 @@ library Secp256k1 {
     // p = 2^256 - 2^32 - 977
     uint256 constant p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
 
+    modifier onlyValidPoint(uint256 x, uint256 y) {
+        if (!isOnCurve(x, y)) revert Secp256k1__InvalidPoint(x, y);
+        _;
+    }
+
     // Check if the point is infinity
     function isInfinity(uint256 x, uint256 y) internal pure returns (bool) {
         return x == 0 && y == 0;
@@ -24,13 +29,35 @@ library Secp256k1 {
         return leftHandSide == rightHandSide;
     }
 
-    function negatePoint(uint256 x, uint256 y) internal pure returns (uint256, uint256) {
-        if (!isOnCurve(x, y)) revert Secp256k1__InvalidPoint(x, y);
+    function negatePoint(uint256 x, uint256 y) internal pure onlyValidPoint(x, y) returns (uint256, uint256) {
+        return negatePointUnchecked(x, y);
+    }
+
+    function negatePointUnchecked(uint256 x, uint256 y) internal pure returns (uint256, uint256) {
         if (isInfinity(x, y)) return (x, y);
         return (x, p - y);
     }
 
-    function addPoint() internal {}
+    function addPoint(uint256 x1, uint256 y1, uint256 x2, uint256 y2)
+        internal
+        pure
+        onlyValidPoint(x1, y1)
+        onlyValidPoint(x2, y2)
+        returns (uint256, uint256)
+    {
+        return addPointUnchecked(x1, y1, x2, y2);
+    }
+
+    function addPointUnchecked(uint256 x1, uint256 y1, uint256 x2, uint256 y2)
+        internal
+        pure
+        returns (uint256, uint256)
+    {
+        if (isInfinity(x1, y1)) return (x2, y2);
+        if (isInfinity(x2, y2)) return (x1, y1);
+        // TODO
+    }
+
     function orderOfPoint() internal {}
     function scalarMultiply() internal {}
 }
